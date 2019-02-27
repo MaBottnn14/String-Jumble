97938632beb39e7cd9e9ee65525d3ccc090ee441
maBottnn14 Hhs <46973490+MaBottnn14@users.noreply.github.com>
2019-02-27 15:06:52 -0500

    Updated from Brython Server: 2/27/2019 3:06:52 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: maBottnn14
+Credit: Mr. Heely, Andrew, and https://developers.google.com/edu/python/lists 
 
 Assignment:
 
@@ -25,3 +25,46 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+m = str(input("Please enter a string of text (the bigger the better): "))
+print("You entered "+m+". Now jumble it: ")
+b =len(m)
+#first line -- searched around after being confused and learned how to reverse a string with help from Mr. Heely(Healy?) 
+def reverse(m):
+    str = ""
+    for i in m:
+        str = i+str 
+    return str
+print(reverse(m))
+
+"""second line -- different method because the previous method produced the string without a space,
+even when I played around with the split and join functions. Andrew taught me how to use the double colon."""
+l =[""]
+o = ""
+for a in m:
+    if a!= " ":
+        o=o+a
+    if a == " ":
+        l.append(o) #https://developers.google.com/edu/python/lists
+        o = ""
+l.append(o)
+b = ' '.join(l[::-1])
+print(b)
+
+#third line -- same method
+y =[]
+h = ""
+for z in m:
+    if z != " ":
+        h = h + z
+    if z == " ":
+        y.append(h[::-1])
+        h = ""
+y.append(h[::-1])
+z = ' '.join(y)
+print(z)
+
+
+
+
+
+
