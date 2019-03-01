0629934bc67009ecc67f23091466c2b028b56fb1
maBottnn14 Hhs <46973490+MaBottnn14@users.noreply.github.com>
2019-03-01 14:08:47 -0500

    Updated from Brython Server: 3/1/2019 2:08:45 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,7 +26,7 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 m = str(input("Please enter a string of text (the bigger the better): "))
-print("You entered "+m+". Now jumble it: ")
+print('You entered "'+m+'". Now jumble it: ')
 b =len(m)
 #first line -- searched around after being confused and learned how to reverse a string with help from Mr. Heely(Healy?) 
 def reverse(m):
@@ -41,7 +41,7 @@ even when I played around with the split and join functions. Andrew taught me ho
 l =[""]
 o = ""
 for a in m:
-    if a!= " ":
+    if a!= "":
         o=o+a
     if a == " ":
         l.append(o) #https://developers.google.com/edu/python/lists
